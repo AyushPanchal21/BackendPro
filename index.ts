56e490cbd63561889c74d85f65e073cef9af0235
AyushPanchal21 <ayupanchal21@gmail.com>
2026-09-09 16:25:18 +0530

    route changes

--- index.ts
+++ index.ts
@@ -4,12 +4,16 @@ import { DBConnect } from './src/service/DataBaseConnection.js'
 import dotenv from "dotenv";
 import { createUser } from "./src/Domain/usecase/CreateUser.js";
 import { loginUser } from "./src/Domain/usecase/LoginUser.js";
+import { loginOrg } from "./src/Domain/usecase/LoginOrg.js";
+import { createorg } from "./src/Domain/usecase/CreateOrg.js"
+import Jwt from "jsonwebtoken";
 
 dotenv.config();
 
 const app = express()
 app.use(express.json())
-const PORT = process.env.Port || 1221;
+const PORT = process.env.PORT;
+const key = process.env.SECRETKEY || ""
 
 DBConnect();
 
@@ -21,6 +25,7 @@ app.post("/create-user", async (req: Request, res: Response) => {
       email,
       password
     )
+    console.log(result)
     if (!result.status) {
       return res.status(409).json(result);
     }
@@ -32,23 +37,76 @@ app.post("/create-user", async (req: Request, res: Response) => {
 })
 
 app.post("/login-user", async (req: Request, res: Response) => {
-  try{
-    const {email,password} = req.body
+  try {
+    const { email, password } = req.body
     const result = await loginUser.execute(
       email,
       password
     )
-    if (!result.status) {
-      return res.status(409).json(result);
+    if (!result.status || !result.user) {
+      return res.status(401).json(result);
     }
-    return res.status(201).json({message:"logged in"});
-  }catch(err){
+    const jwttoken = Jwt.sign(
+      {
+        orgId: result.user.id,
+      },
+      key
+    )
+    return res.status(201).json({ message: "logged in",orgtoken:jwttoken });
+  } catch (err) {
     console.log(err)
   }
 })
 
-app.post("/login-organization", async (req, res) => { })
+app.post("/create-org", async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const orgResult = await createorg.execute(
+      name,
+      email,
+      password
+    );
+
+    if (!orgResult.status) {
+      return res.status(409).json(orgResult);
+    }
+
+    return res.status(201).json(orgResult);
+
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).json({
+      message: "Internal server error"
+    });
+  }
+});
+
+app.post("/login-organization", async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body
+    const result = await loginOrg.execute(
+      email,
+      password
+    )
+    if (!result.status || !result.user) {
+      return res.status(401).json(result);
+    }
+
+    const jwttoken = Jwt.sign(
+      {
+        orgId: result.user.id,
+      },
+      key
+    )
+    return res.status(201).json({ message: "logged in",orgtoken:jwttoken });
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`) // undefined
+  console.log(`Example app listening on port ${PORT}`) 
 })
