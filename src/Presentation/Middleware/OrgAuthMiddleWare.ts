9606500acac69379d68ad3456993a8341855c884
AyushPanchal21 <ayupanchal21@gmail.com>
2026-09-09 16:19:22 +0530

    middleware addition

---
+++ src/Presentation/Middleware/OrgAuthMiddleWare.ts
@@ -0,0 +1,46 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface OrgJwtPayload {
+    userId: string;
+    orgId?: string;
+    orgOwner?: boolean;
+}
+
+const JWTKEY = process.env.SECRETKEY || ""
+
+export const OrgAuth = (req: Request, res: Response, next: NextFunction)=>{
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            return res.status(401).json({
+                message: "Authorization token missing"
+            });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({
+                message: "Token missing"
+            });
+        }
+
+         const decoded = jwt.verify(
+            token,
+            JWTKEY
+        ) as OrgJwtPayload;
+
+
+        req.user = decoded;
+
+        next();
+
+    } catch (error) {
+        return res.status(401).json({
+            message: "Invalid or expired token"
+        });
+    }
+}
+
