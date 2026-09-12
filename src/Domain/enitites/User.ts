3fc2b10966fed846ce54f979ed34eefa0280f413
AyushPanchal21 <ayupanchal21@gmail.com>
2026-09-12 18:54:00 +0530

    added user input fields

--- src/Domain/enitites/User.ts
+++ src/Domain/enitites/User.ts
@@ -1,7 +1,21 @@
+export enum Role{
+    Candidate = "Candidate",
+    Manager = "Manager",
+    TechGuy = "TechGuy",
+    Sales = "Sales",
+    ResourcesGuy = "ResourcesGuy"
+}
+
 export interface User {
-    id:string,
     name: string,
     email : string,
     password : string,
+    role:Role
     createdAt : Date,
+}
+
+export interface InputUserField{
+    name: string,
+    email : string,
+    password : string,
 }
