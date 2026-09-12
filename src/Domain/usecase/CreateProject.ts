861cc688fa2c3f71f5ed3ff9e94f15ac39df4091
AyushPanchal21 <ayupanchal21@gmail.com>
2026-09-12 18:55:34 +0530

    modified to take project input fields

--- src/Domain/usecase/CreateProject.ts
+++ src/Domain/usecase/CreateProject.ts
@@ -1,27 +1,42 @@
 import type { ProjectRepository } from "../repository/projectRepository.js";
 import { MongoProjectRepo } from "../../Data/repository/MongoProjectRepo.js";
 import { Status } from "../enitites/Project.js";
+import type { Project ,InputProjectFields} from "../enitites/Project.js";
 
 export class CreateProject {
+
     private projectRepository: ProjectRepository;
     constructor(projectRepository: ProjectRepository) {
         this.projectRepository = projectRepository;
     }
-    async execute(id:string){
-        const project = await this.projectRepository.findProjectId(id)
+
+    async execute(projectvariable: InputProjectFields) {
+        const project = await this.projectRepository.findProject(projectvariable.orgId,projectvariable.proName)
         if (project) {
             return {
                 status: false,
                 message: "project exists exists!",
             }
         }
-        else{
-            // const newproject = await this.projectRepository.createProject({})
+        else {
+            const newproject = await this.projectRepository.createProject({
+                orgId: projectvariable.orgId,
+                proName: projectvariable.proName,
+                description: projectvariable.description,
+                allMem: [],
+                createdAt: new Date(),
+                status: Status.NotStarted,
+            })
             return {
                 status: true,
-                message: "new project created"
+                message: "new project created",
+                project:newproject,
             }
         }
     }
 }
+
+const projectrepo = new MongoProjectRepo()
+export const createproject = new CreateProject(projectrepo)
+
 
