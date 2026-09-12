a90ec721285d9c174021b331177c01f189822fe8
AyushPanchal21 <ayupanchal21@gmail.com>
2026-09-12 18:58:13 +0530

    added find and create project methods

--- src/Domain/repository/projectRepository.ts
+++ src/Domain/repository/projectRepository.ts
@@ -1,6 +1,6 @@
 import type{Project} from "../enitites/Project.js"
 
 export interface ProjectRepository{
-    findProjectId(id:string):Promise< Project | null>
+    findProject(orgid: String,proname:String):Promise< Project | null>
     createProject(pro:Project):Promise<Project>
 }
