05d4ea1c49eafa03587fc19dc53843d4fc78d0f3
AyushPanchal21 <ayupanchal21@gmail.com>
2026-09-17 02:10:04 +0530

    implementatino

--- src/Domain/repository/applicationRepository.ts
+++ src/Domain/repository/applicationRepository.ts
@@ -3,4 +3,5 @@ import type { Application } from "../enitites/Application.js";
 export interface ApplicationRepository {
     createApplication(application: Application): Promise<Application>
     checkApplication(userid: string,orgid:string): Promise<Application | null>
+    getApplication(orgid:string): Promise<Application[] | null>
 }
