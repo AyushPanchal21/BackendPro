3f2bf79ab06049d4e4ec5a805e451c5b05caa597
AyushPanchal21 <ayupanchal21@gmail.com>
2026-09-17 01:49:52 +0530

    query interface add

--- src/Domain/enitites/Application.ts
+++ src/Domain/enitites/Application.ts
@@ -4,9 +4,21 @@ export enum Status{
     Accepted="Accepted",
 }
 
-export interface ApplicationFields{
+export interface Application{
+    orgid:string,
+    userid:string,
+    role:string,
+    experience:string,
+    status:Status
+}
+export interface ApplicationInputFields{
     orgid:string,
     userid:string,
     role:string,
     experience:string
+}
+
+export interface ApplicationQuery {
+    userid: string
+    orgid: string
 }
