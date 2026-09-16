a03f49d9cc7d9ce17943d6ecbb0f622ac3302b10
AyushPanchal21 <ayupanchal21@gmail.com>
2026-09-17 01:46:39 +0530

    main methods for app

---
+++ src/Data/repository/MongoAppRepo.ts
@@ -0,0 +1,14 @@
+import { ApplicationModel } from "../Models/ApplicationSchema.js"
+import type { ApplicationRepository } from "../../Domain/repository/applicationRepository.js"
+import type { Application } from "../../Domain/enitites/Application.js"
+
+export class MongoAppRepo implements ApplicationRepository {
+    async createApplication(application: Application) {
+        const newapplication = new ApplicationModel(application)
+        return await newapplication.save()
+    }
+
+    async checkApplication(userid: string,orgid:string) {
+        return await ApplicationModel.findOne({ userid ,orgid})
+    }
+}
